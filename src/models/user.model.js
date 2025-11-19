f7f85ad1627727936cb48315687ad2a7933f068c
unknown <danielbusiness859@gmail.com>
2025-11-19 18:49:18 +0000

    COORECTION DE BYCRYPT

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 // ==========================================
 // 🔹 Définition du schéma utilisateur
