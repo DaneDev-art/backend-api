f9cdf8dbd124006870221f2df845fa788eda910d
unknown <danielbusiness859@gmail.com>
2025-12-14 14:36:31 +0000

    VERIFICATION MAIL ET CONNEXION

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { verifyToken, verifyAdmin } = require("../middleware/auth.middleware");
-const authController = require("../controllers/authController");
+const authController = require("../controllers/auth.controller");
 
 const router = express.Router();
 
